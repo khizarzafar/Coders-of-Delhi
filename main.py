a41895db71ac595ca1fd7adb7216c1060cd7852c
Your Name <khizarzafar19@gmail.com>
2025-07-31 20:25:38 +0500

    display data

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import json
 
-def load_data(filename):
-    with open(filename,"r") as f:
-        data = json.load(f)
-    return data
 
-data = load_data("data.json")
-print(data)
+# Function to load data from json file
+# def load_data(filename):
+#     with open(filename,"r") as f:
+#         data = json.load(f)
+#     return data
+
+# data = load_data("data.json")
+
+#Function to display the users and their liked pages
+# def display_users(data):
+#     print("\nUsers connections:")
+#     for user in data['users']:
+#         print(f"ID:{user['id']} - {user['name']} is friends with {user['friends']} and their liked pages are {user['liked_pages']}.")
+#     print("\nPages information:")
+#     for page in data['pages']:
+#         print(f"{page['id']}: {page['name']}")
+
+# display_users(data)
