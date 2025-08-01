4b2a96fab28a927e714b21b0db6d1fa3d7fce558
Your Name <khizarzafar19@gmail.com>
2025-08-01 22:50:40 +0500

    step1

--- people_youMayKnow.py
+++ people_youMayKnow.py
@@ -7,7 +7,12 @@ def data_load(filename):
 
 
 def find_people_you_may_know(user_id,data):
-    pass
+    #Step 1: We find all friends of user_id 1 and and store them in user_friends dictionary...
+    user_friends = {}
+    for user in data['users']:
+        user_friends[user['id']] = set(user['friends']) 
+#   After this loop, the user_friends dictionary looks like this:
+#   {1: {2, 3}, 2: {1, 4}, 3: {1}, 4: {2}, 5: set()}
 
 
 data = data_load("data.json")
