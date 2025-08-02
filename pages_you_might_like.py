54e5c386ad5aa47305936172f544369e7e9cf532
Your Name <khizarzafar19@gmail.com>
2025-08-02 23:20:18 +0500

    user liked pages

--- pages_you_might_like.py
+++ pages_you_might_like.py
@@ -6,3 +6,21 @@ def load_data(filename):
     
 def pages_you_might_like(user_id,data):
     #dict to store user interacted pages
+    user_pages = {}
+    for user in data['users']:
+        user_pages[user['id']] = set(user['liked_pages'])
+        # After all iterations, user_pages becomes:
+        # {
+        #     1: {101},
+        #     2: {102},
+        #     3: {101, 103},
+        #     4: {104}
+        # }
+
+    if user_id not in user_pages:
+        return []
+    
+    user_liked_pages = user_pages[user_id]
+    # user_liked_pages is now {101}.
+
+    
