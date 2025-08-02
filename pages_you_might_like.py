3d8bb788ea0fe3aba3f13b5d0b991bde78706c11
Your Name <khizarzafar19@gmail.com>
2025-08-02 23:53:56 +0500

    pages liked

--- pages_you_might_like.py
+++ pages_you_might_like.py
@@ -31,5 +31,7 @@ def pages_you_might_like(user_id,data):
     #other_user will be the user ID, and pages will be the set of pages they liked.
         if other_user != user_id:
             shared_pages = user_liked_pages.interaction(pages)
-
-
+            for page in pages:
+                if page not in user_liked_pages:
+                    page_suggestions[page] = page_suggestions.get(page,0) + len(shared_pages)
+                    
