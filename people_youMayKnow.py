694bbc9d28a007528a989baf07f9fc8389da02ee
Your Name <khizarzafar19@gmail.com>
2025-08-02 13:27:24 +0500

    friends of friends

--- people_youMayKnow.py
+++ people_youMayKnow.py
@@ -12,22 +12,26 @@ def find_people_you_may_know(user_id,data):
     user_friends = {}
     for user in data['users']:
         user_friends[user['id']] = set(user['friends']) 
-#   After this loop, the user_friends dictionary looks like this:
-#   {1: {2, 3}, 2: {1, 4}, 3: {1}, 4: {2}, 5: set()}
+    #   After this loop, the user_friends dictionary looks like this:
+    #   {1: {2, 3}, 2: {1, 4}, 3: {1}, 4: {2}, 5: set()}    
 
     if user_id not in user_friends:
         return []
     
     #!Step 2: Identifying direct friends...
     direct_friends = user_friends[user_id]
-#   This line retrieves the set of friends for user_id 1
-#   direct_friends is now {2,3}
+    #   This line retrieves the set of friends for user_id 1
+    #   direct_friends is now {2,3}
+
     suggestions = {}
-#   An empty dict to store mutual friend counts
+    #   An empty dict to store mutual friend counts
 
-
-
-
+    #!Step 3: Finding Friends of Friends
+    for friend in direct_friends:
+        for mutual in user_friends[friend]:
+            if mutual != user_id and mutual not in direct_friends:
+                suggestions[mutual] = suggestions.get(mutual,0) + 1
+                #   The suggestions dictionary becomes {4: 1}
 
 
 
